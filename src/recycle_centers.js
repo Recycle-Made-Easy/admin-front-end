9d2148963c63bf206ac8a2586a0d366296b9d635
Diana McIntosh <dotteddice@yahoo.com>
2019-12-06 14:15:31 -0500

    edit recycle center is working

--- src/recycle_centers.js
+++ src/recycle_centers.js
@@ -131,7 +131,7 @@ module.exports = {
                         headers: {
                             "Content-Type": "application/json"
                         },
-                        body: toStringify({
+                        body: JSON.stringify({
                             name: inputName.value,
                             streetAddress: inputStreetAddress.value,
                             city: inputCity.value,
